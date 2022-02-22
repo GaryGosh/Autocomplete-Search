c857ca50218f06b3dab51cea776e2a34c654e6cb
neeraj_tk <neeraj.tk@moonraft.com>
2022-02-23 00:55:06 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import BookData from "./Data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <img src={logo} className="App-logo" alt="logo" /> */}
+			<SearchBar placeholder="Type a Book Name.." data={BookData} />
+		</div>
+	);
 }
 
 export default App;
